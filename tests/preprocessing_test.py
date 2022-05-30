4ac6f6ea1cfb6ac95144e2198fec067ebf58e60c
Alfonso Ancarola <alfonso.ancarola.py@gmail.com>
2022-05-30 15:42:30 -0300

    enable CI

--- tests/preprocessing_test.py
+++ tests/preprocessing_test.py
@@ -1,33 +1,9 @@
-from building_toolbox.preprocesing import *
-
-def test_importer():
-    """
-    Test the importer function
-    """
-    # Test the importer function
-    path = 'tests/test_data/'
-    file_name = 'test_data.csv'
-    data = importer(path, file_name)
-    assert data.shape == (3, 3)
+from buliding_toolbok.preprocessing import importing_data, duplicate_count, drop_duplicates
+import pandas as pd
 
 def test_duplicate_count():
     """
-    Test the duplicate_count function
+    Test the duplicate_count function.
     """
-    # Test the duplicate_count function
-    path = 'tests/test_data/'
-    file_name = 'test_data.csv'
-    data = importer(path, file_name)
-    count = duplicate_count(data)
-    assert count == 2
-
-    def test_drop_duplicates():
-        """
-        Test the drop_duplicates function
-        """
-        # Test the drop_duplicates function
-        path = 'tests/test_data/'
-        file_name = 'test_data.csv'
-        data = importer(path, file_name)
-        data = drop_duplicates(data)
-        assert data.shape == (2, 3)
+    data = pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6], 'c': [7, 8, 9]})
+    assert duplicate_count(data) == 0
